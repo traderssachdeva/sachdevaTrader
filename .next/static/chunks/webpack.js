bb69f177556b8431f13c7729750a441c2352da02
sachdeva <traderssacheva0@gmail.com>
2023-04-10 01:58:01 -0700

    added functionality

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fcada9fc62aa943c"; }
+/******/ 		__webpack_require__.h = function() { return "4dbd6ff01bfce065"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/OrderNow.js
+++ components/OrderNow.js
@@ -1,10 +1,56 @@
 import React from "react";
-
+import emailjs from "@emailjs/browser";
+import { useForm } from "react-hook-form";
 function OrderNow() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const [input, setInput] = useState("");
+  // const notify = () => toast.success('Copied')
+
+  //   const onSubmit = (data) => console.log(data)
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    // e.preventDefault();
+    emailjs
+      .sendForm(
+        'service_maqrkn6',
+        'template_9ah7vhg',
+        // your service id
+        // your template id
+
+        form.current,
+        'JxnW1-P4KX7EK_Spx'
+        // your api key of email js
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          reset();
+          // toast.success("Message sent");
+        },
+        (error) => {
+          //   console.log(error.text)
+        }
+      );
+
+    const reset = (e) => {
+      setInput("");
+    };
+  };
   return (
     <div>
       <div className=" grid  grid-cols-1 md:grid-cols-3 bg-white py-8 px-6 sm:py-10 lg:px-8">
-        <form className="mx-auto col-span-2 mt-4 max-w-xl sm:mt-4">
+        <form ref={form}
+              value={input}
+              onChange={(e) => setInput()}
+              onSubmit={handleSubmit(sendEmail)} className="mx-auto col-span-2 mt-4 max-w-xl sm:mt-4">
           <div className="mx-auto my-20  max-w-2xl text-center">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
               Order Now
@@ -17,30 +63,16 @@ function OrderNow() {
               </label>
               <div className="mt-2.5">
                 <input
-                  type="text"
+                value={input}
+                onChange={(e) => setInput()}
+                {...register("name", { required: true })}
+                type="text"
+                  
                   className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
-            <div className="sm:col-span-2">
-              <label
-                
-                className="block text-sm font-semibold leading-6 text-gray-900"
-              >
-                Phone number
-              </label>
-              <div className="relative mt-2.5">
-                <div className="absolute inset-y-0 left-0 flex items-center">
-                  <select className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
-                    <option>+ 91</option>
-                  </select>
-                </div>
-              </div>
-              <input
-                type="number"
-                className="block w-full rounded-md border-0 py-2 px-3.5 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+
           </div>
           <div className="sm:col-span-2">
             <div className="sm:col-span-2">
@@ -49,6 +81,10 @@ function OrderNow() {
               </label>
               <div className="mt-2.5">
                 <input
+                value={input}
+                onChange={(e) => setInput()}
+                {...register("email", { required: true })}
+                name="email"
                   type="email"
                   className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -62,7 +98,11 @@ function OrderNow() {
               </label>
               <div className="mt-2.5">
                 <textarea
-                  name="message"
+                value={input}
+                onChange={(e) => setInput()}
+                name="message"
+                {...register("message", { required: true })}
+                
                   id="message"
                   rows="4"
                   className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"

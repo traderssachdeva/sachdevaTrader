1be8979ee6e809f025b704d58e8d83d2d8fe6bd0
sachdeva <traderssacheva0@gmail.com>
2023-04-10 02:01:19 -0700

    bug solved

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4dbd6ff01bfce065"; }
+/******/ 		__webpack_require__.h = function() { return "2143bd9eb0cddb7a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/OrderNow.js
+++ components/OrderNow.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import emailjs from "@emailjs/browser";
 import { useForm } from "react-hook-form";
 function OrderNow() {
